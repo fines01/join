20e0cc3acebd3e3c1598fac1f1949334e1bff609
I. Fritsch <ines.fritsch@gmail.com>
2022-04-21 19:23:08 +0200

    Add general functions

---
+++ script.js
@@ -0,0 +1,23 @@
+// generic functions
+// 
+function getId(el) {
+    return document.getElementById(el);
+}
+
+function hide(...elements) { // spread-operator
+    for (let i = 0; i < elements.length; i++) {
+        getId(elements[i]).classList.add('d-none');
+    }
+}
+
+function show(...elements) {
+    for (let i = 0; i < elements.length; i++) {
+        getId(elements[i]).classList.remove('d-none');
+    }
+}
+
+function toggle(...elements) {
+    for (let i = 0; i < elements.length; i++) {
+        getId(elements[i]).classList.toggle('d-none');
+    }
+}
