1f3a44aa70d929929c17a6e1d352cbb83ffc1dfd
unknown <sth1812@posteo.de>
2022-04-25 04:21:16 +0200

    addTask ready

--- js/script.js
+++ js/script.js
@@ -35,31 +35,51 @@ id="date"
 
 
 
-/* addToTaskJS the function is meant to enable the add of task to a json array*/
-
-
+/* addToTaskJS the function is meant to enable the add of task to a json array
+*/
+let tasks = [];
 function addToTask() {
-    let title = getId('title');
-    let discription = getId('description');
-    let category = ['management', 'production'];
-    let urgency = ['low', 'middle', 'high'];
 
-    let addToTask = {
+    let title = document.getElementById('title');
+    let description = document.getElementById('description');
+    let category = document.getElementById('category');
+    let urgency = document.getElementById('urgency');
+    let date = document.getElementById('date');
+
+    let task = {
         'title': title.value,
-        'dueDate': dueDate,
-        'category': category,
-        'urgency': urgency,
-        'description': description.value
+        'description': description.value,
+        'category': category.value,
+        'urgency': urgency.value,
+        'date': date.value
     };
 
+    tasks.push(task);
+    saveTasks();
     title.value = '';
-    discription.value = '';
+    date.value = '';
+    category.value = '';
+    urgency.value = '';
+    description.value = '';
+}
+
+/* The function is used to save the tasks in a JSON Array from the addTask function in a string */
+
+function saveTasks() {
+    let tasksAsText = JSON.stringify(tasks);
+    localStorage.setItem('tasks', tasksAsText);
+}
+
+/* The function is used to laod the tasks from text-format to a JSON-array */
+
+function loadTasks() {
+    let tasksAsText = localStorage.getItem('tasks');
+    if (tasksAsText) {
+        tasks = JSON.parse(tasksAsText);
+    }
 }
 
 
-function renderAddToTask() {
-  
-}
 
-/* JSON for addToTask Page  */
+
 
