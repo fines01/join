01c33936e3b39bf03b8a58d0b76d26c8fb781cf8
Marco <marco.beer93@web.de>
2022-04-28 21:04:46 +0200

    added data.js

--- js/script.js
+++ js/script.js
@@ -17,21 +17,6 @@ id="done"
 /* ********* global scope - here we define global variables and constants *********  */
 
 // example data for testing purposes
-let tasks = [
-    { title: 'a', description: 'a', category: 'Management', urgency: 'high', date: '2022-04-20' },
-    { title: 'b', description: 'b', category: 'Management', urgency: 'intermediate', date: '2022-04-27' },
-    { title: 'c', description: 'c', category: 'Management', urgency: 'low', date: '2022-04-27' },
-    { title: 'd', description: 'd', category: 'Management', urgency: 'High', date: '2022-04-27' },
-    { title: 'e', description: 'e', category: 'Management', urgency: 'High', date: '2022-04-27' },
-    { title: 'f', description: 'f', category: 'Management', urgency: 'High', date: '2022-04-27' }
-];
-// example data for testing purposes
-let members = [
-    { name: 'Jolene Bauer', email: 'jo@test.at', color: '', img: '' },
-    { name: 'Jimmmy Dude', email: 'dude@company.com', color: '', img: '' },
-    { name: 'Max Mo', email: 'donot@email.me', color: '', img: '' },
-    { name: 'Maja', email: 'maja@muster.mix', color: '', img: '' },
-]
 
 let currentDraggedElement;
 
