1e500e45eb4bb3de57083786865bce4d7e4eb03f
Marco <marco.beer93@web.de>
2022-05-06 18:42:46 +0200

    cleaned code on backlog JS

--- js/backlog.js
+++ js/backlog.js
@@ -3,23 +3,6 @@
 /**
  * Renders all logs from the tasks array
  */
-function renderLogss() {
-    let logs = getId('logs');
-    logs.innerHTML = '';
-
-    for (let i = 0; i < tasks.length; i++) {
-        // for testing purposes: [null] error
-        if (!tasks[i].assignedTo || tasks[i].assignedTo.length < 1 || tasks[i].assignedTo[0] == null) {
-            tasks[i].assignedTo = "Unassigned";
-        }
-        //
-        logs.innerHTML += logsHTML(tasks[i]);
-    }
-}
-
-
-
-
 function backlogToBoard(i) {
     tasks[i]['board'] = 'todo'
     renderLogs()
@@ -46,3 +29,18 @@ function deleteBacklogTask(i) {
     saveTasks();
 }
 
+
+
+//function renderLogss() {
+//let logs = getId('logs');
+//logs.innerHTML = '';
+
+//for (let i = 0; i < tasks.length; i++) {
+// for testing purposes: [null] error
+// if (!tasks[i].assignedTo || tasks[i].assignedTo.length < 1 || tasks[i].assignedTo[0] == null) {
+//   tasks[i].assignedTo = "Unassigned";
+//}
+//
+//logs.innerHTML += logsHTML(tasks[i]);
+//}
+//}
